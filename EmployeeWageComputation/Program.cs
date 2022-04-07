e09b37e1d6ca1f5f9bbdd2cf4ad21a357983b3fe
Vidyashree N M <vidyashree2109@gmail.com>
2022-04-07 23:49:35 +0530

    [Vidya] Add : Employee attendance implemented

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EmployeeWageComputation;
+using System;
+EmployeeAttendance employee=new EmployeeAttendance();
+employee.EmployeeAttendence();
