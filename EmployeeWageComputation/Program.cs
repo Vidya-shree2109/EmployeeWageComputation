030b5045ad4df0c3e7c1ee292e3274f5d2445bd9
Vidyashree N M <vidyashree2109@gmail.com>
2022-04-20 19:02:41 +0530

    [Vidya] Refactored : Saved The Total Wage Of Employee For Each Company

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -1,6 +1,11 @@
 ﻿using EmployeeWageComputation;
 
-EmployeeWageUsingClass employee = new EmployeeWageUsingClass();
-employee.Company("Samsung", 20, 4, 20);
-employee.Company("Reliance", 10, 4, 20);
-employee.Company("Airtel", 30, 4, 20);
+EmployeeWageUsingClass samsung = new EmployeeWageUsingClass("Samsung", 20, 5, 10);
+EmployeeWageUsingClass reliance = new EmployeeWageUsingClass("Reliance", 10, 4, 10);
+EmployeeWageUsingClass airtel = new EmployeeWageUsingClass("Airtel", 30, 5, 20);
+samsung.ComputeEmployeeWage();
+Console.WriteLine(samsung.displaySalary() + "\n");
+reliance.ComputeEmployeeWage();
+Console.WriteLine(reliance.displaySalary() + "\n");
+airtel.ComputeEmployeeWage();
+Console.WriteLine(airtel.displaySalary() + "\n");
