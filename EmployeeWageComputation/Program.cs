b7e10e9363a05cd6a21914a7d52d2352ab82d9d7
Vidyashree N M <vidyashree2109@gmail.com>
2022-04-26 23:26:56 +0530

    [Vidya] Refactored : Managing Employee Wage Using Array List

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -5,13 +5,6 @@ class Program
     public static void Main(String[] args)
     {
         WageComputation company = new WageComputation();
-        Console.WriteLine("Enter :\nThe Company Name\nEmpWage Working Per Hours\nTotal Working Hours\nTotal Working Days In Month");
-        string compName = Console.ReadLine();
-        int EmpWageWorkingPerHours = Convert.ToInt32(Console.ReadLine());
-        int TotalWorkingHours = Convert.ToInt32(Console.ReadLine());
-        int TotalWorkingDaysInMonth = Convert.ToInt32(Console.ReadLine());
-        company.AddCompanyEmpWage(compName, EmpWageWorkingPerHours, TotalWorkingHours, TotalWorkingDaysInMonth);
         company.ComputeEmpWage();
-        Console.WriteLine("Total wage for" + compName + ":" + " " + company.GetTotalEmpWage(compName));
     }
 }
